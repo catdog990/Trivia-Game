0d14a4e37695c033a0d1eeeda784e6970fd260d0
catdog990 <ozmcastro@gmail.com>
2017-12-14 09:35:09 -0500

    questions in progress

---
+++ assets/javascript.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+var timer = 6;
+
+var intervalId;
+
+$("#impButton").on("click", run);
+
+function run() {
+	intervalId = setInterval(decrement, 1000);
+
+// ***Questions will appear when button is clicked***///
+	var html =
+
+		"<p> Question 1: What is Michael's Last Name?</p>" +
+		"<p> </p>";
+
+		document.querySelector("#firstQ").innerHTML = html;
+}
+
+
+
+function decrement(){
+	timer--;
+
+	$("#Clock").html("<h2>Time Left: " + timer + " Seconds</h2>");
+
+
+	if (timer === 0) {
+		stop();
+	}
+}
+
+function stop() {
+	clearInterval(intervalId);
+}
+
+
+
+//  $("#impButton").click(function(){
+// 	var html =
+
+// 		"<p> Question 1:</p>" +
+// 		"<p> What is a goobber?</p>";
+
+// 		document.querySelector("#wut").innerHTML = html;
+// });
+
+});
+// ****On the backburner, trying another method***///
+// setTimeout(fiveSeconds, 1000 * 5);
+
+// function fiveSeconds(){
+// 	$("#Clock").append("<h2>wut</h2>");
+	
+// }
+
+
+
+
+// *****Start of Questions*****/////
+
+var position = 0;
+var correct = 0;
+var choices = 0;
+var A="";
+var B="";
+var C="";
+var D="";
+
+var Questions = [
+	["What is Michael's Last Name?", "Jordan", "McCringleBerry", "Pepperwood","Scott", "D"],
+	["Bears, Beets, ?", "Battleships", "Boyz N the Hood", "Battlestar Galactica", "China", "C"],
+	["Michael's Nickname is?", "Michael Klump", "Jackhammer", "Michael Scarn", "Sebring Seranator", "A"],
+	["Dwight's Choice of Crop is", "Potatos", "Beets", "Garden Gnomes", "Corn", "B"],
+	["Jim is to Pam, as Dwight is to", "Esther", "Meredith", "Kelly", "Angela", "D"]
+];
+
+// function get(x){
+// 	return.document.getElementbyId(x);
+// }
