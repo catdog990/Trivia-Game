456502ad2a5325f2a6cdaf19f880ab9f92f8a223
catdog990 <ozmcastro@gmail.com>
2017-12-15 00:29:46 -0500

    Im Lost

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,19 +3,18 @@ $(document).ready(function(){
 var timer = 6;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
+var unanswered = 0;
 var Questions = ["What is Michael's Last Name?", "Bears, Beets, ?", "Michael's Nickname is?", "Dwight's Choice of Crop is", "Jim is to Pam, as Dwight is to"];
-var Answers = [""]
+var Answers = ["Scott", "Battlestar Galactica", "Michael Klump", "Beets", "Angela"];
 var intervalId;
 
+
+//*****Timer******/////
 $("#impButton").on("click", run);
 
 function run() {
 	intervalId = setInterval(decrement, 1000);
-
-// ***Questions will appear when button is clicked***///
 }
-
-
 
 function decrement(){
 	timer--;
@@ -30,6 +29,15 @@ function decrement(){
 
 function stop() {
 	clearInterval(intervalId);
+}
+
+///********Questions*******///
+for (var i = 0; i < Questions.length; i++){
+	console.log("wut is dis!" + Questions[i]);
+}
+
+for (var i = 0; i < Answers.length; i++){
+	console.log(Answers[i]);
 }
 
 });
